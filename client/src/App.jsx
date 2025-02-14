746dbb20e4f25554dcab146cf2b8fd23913e646b
Donghoon Seo <naosdh@naver.com>
2025-02-14 19:38:07 +0900

    Complete to make Channel Container

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components'
 
+import 'stream-chat-react/dist/css/v2/index.css';
 import './App.css';
 
 const cookies = new Cookies();
@@ -26,16 +27,27 @@ if (authToken) {
 }
 
 const App = () => {
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) return <Auth />
 
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
         <ChannelListContainer
-
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
         />
         <ChannelContainer
-
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
         />
       </Chat>
     </div>
